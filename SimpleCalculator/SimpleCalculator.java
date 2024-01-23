c89e53ccc40f0a33309d64cde5727a7d4e5f0625
David Gelman <gelmands@college.omsu.ru>
2024-01-23 22:02:46 +0600

    Simple Calculator for a three operations

---
+++ SimpleCalculator/SimpleCalculator.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class SimpleCalculator{
+    public static void main(String[] args)
+    {
+        //Приветствие
+        System.out.print("""
+                Здравствуйте.для расчета значений введите данные в формате:
+                Попробуйте еще раз. Введите данные в формате [операнд1] [оператор] [операнд2].
+                Операнд может быть: +, -, *, : или /В качестве операндов принимаются любые числа""");
+
+        //Переменные
+        double firstOperand;
+        double secondOperand;
+        double operationResult=0;
+        String operator;
+
+        //Объект для чтения данных со стандартным системным потоком
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("\nВаш ввод: ");
+        try {
+            //Чтение данных
+            firstOperand = input.nextDouble();
+            operator = input.next();
+            secondOperand = input.nextDouble();
+
+            switch (operator) {
+                case "+":
+                    operationResult = firstOperand + secondOperand;
+                    break;
+                case "-":
+                    operationResult = firstOperand - secondOperand;
+                    break;
+                case "*":
+                    operationResult = firstOperand * secondOperand;
+                    break;
+                case ":":
+                case "/":
+                    operationResult = firstOperand / secondOperand;
+                    break;
+                default:
+                    System.out.print("""
+                            Ввод данных не по формату.
+                            Вводите данные в формате [операнд1] [оператор] [операнд2].
+                            Операнд может быть: +, -, *, : или /. В качестве операндов принимаются любые числа
+                            Обращайте внимание на наличие пробелов между операторами и операндами"""
+                    );
+            }
+            System.out.printf("Результат операции: %.2f", operationResult);
+        } catch (Exception e) {
+            System.out.print("""
+                            Ввод данных не по формату.
+                            Вводите данные в формате [операнд1] [оператор] [операнд2].
+                            Операнд может быть: +, -, *, : или /. В качестве операндов принимаются любые числа
+                            Обращайте внимание на наличие пробелов между операторами и операндами"""
+            );
+        }
+    }
+}
